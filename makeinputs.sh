6e0b0cae1df50658fc13bd645fdf41628108d3bd
(no author) <(no author)@57e68679-7564-4017-a3af-2a4836dcd6fc>
2011-03-17 12:22:35 +0000

    Changed version 'source', changed categories for circlethrow, added focusedwalking
    git-svn-id: https://svn.see.ed.ac.uk/svn/mrichar1@297 57e68679-7564-4017-a3af-2a4836dcd6fc

--- makeinputs.sh
+++ makeinputs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ls -1 *.tex |perl -ne 'next if ($_ =~ /^games.tex$/ or /^inputs.tex$/ or /^title.tex$/); s/\.tex$//; s/(.*)/\\input{$1}/; print "$_";'
 #Get svn version for games.pdf
-svn info games.tex |grep "Last Changed Rev" |awk '{print $4}' >version.tex
+svn info . |grep "Last Changed Rev" |awk '{print $4}' >version.tex
